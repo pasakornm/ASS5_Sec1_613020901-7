69e4ed89483085c8368de5f287c6f64be670ba14
pasakornm <pasakornm@kkumail.com>
2020-09-06 16:59:44 +0700

    ASS5_Sec1
    613020901-7

---
+++ LabAss5Intent/app/src/main/java/com/example/labass5intent/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.labass5intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        onClickSendData()
+        onClickReset()
+    }
+    fun onClickSendData(){
+        btnSend.setOnClickListener{
+            val selectID:Int = radioGroup.checkedRadioButtonId
+            val checkedRadioText: RadioButton = findViewById(selectID)
+            val intent = Intent(this, secondActivity::class.java)
+            intent.putExtra("employee", Employee(edit_name.text.toString(), checkedRadioText.text.toString(), editMail.text.toString(), editSalary.text.toString().toInt()))
+            startActivity(intent)
+        }
+    }
+    fun onClickReset(){
+        btnReset.setOnClickListener{
+            edit_name.text?.clear()
+            editMail.text?.clear()
+            editSalary.text.clear()
+            radioGroup.clearCheck()
+        }
+    }
+}
